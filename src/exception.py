ba0414f3c4d70702e42a683e40e007df58594646
Alexandra Penin <alexandra.cecile.penin@gmail.com>
2023-09-29 18:02:04 +0200

    EDA and model

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging #not need to have from src.logger import logging if it's in the same folder !
+from src.logger import logging#not need to have from src.logger import logging if it's in the same folder !
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -14,12 +14,3 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-    
-
-#if __name__=="__main__":
-#
-#    try:
-#        a=1/0
-#    except Exception as e:
-#        logging.info("Divide by Zero")
-#        raise CustomException(e, sys)
