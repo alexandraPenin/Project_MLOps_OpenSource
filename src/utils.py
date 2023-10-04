f09387c44ae2ada6d3078bb2970363973e4d276d
Alexandra Penin <alexandra.cecile.penin@gmail.com>
2023-10-04 17:46:31 +0200

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import dill
 
 import numpy as np
 import pandas as pd
+import pickle
 
 from src.exception import CustomException
 
@@ -52,5 +53,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
         return report
     
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
